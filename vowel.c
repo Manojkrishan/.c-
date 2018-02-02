02351d3c913079731a37023d925788e5aec20af0
Manojkrishan <35995028+Manojkrishan@users.noreply.github.com>
2018-02-02 14:18:09 +0530

    vowel

---
+++ vowel.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+char a;
+printf("enter any letter");
+scanf("%c",&a);
+if(a=='a' a=='e' a=='i' a=='o' a=='u')
+printf("vowel");
+else
+printf("consonant");
+return 0;
+}
